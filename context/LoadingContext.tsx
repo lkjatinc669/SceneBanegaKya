b3593365a50d819eca02662fb00abdede8cfcc7e
lkjgclabs <lkjgclabs@gmail.com>
2026-07-06 17:44:23 +0530

    Changing versions

--- context/LoadingContext.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { createContext, useContext } from "react";
-
-export const LoadingContext = createContext(false);
-
-export const useLoading = () => useContext(LoadingContext);
